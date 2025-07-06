60e9628925d966c1a45f50e5324e25e530e299f8
Adrian <adrianhyk@gmail.com>
2025-07-07 01:03:33 +0800

    wip

--- _sample/.kypanel/entry.ts
+++ _sample/.kypanel/entry.ts
@@ -11,6 +11,16 @@ setup({
    moduleFormat: 'esm',
    compileMode: {
       dbModules: [
+         // TODO: maybe use TS to ensure that relative imports always have leading './'
+         // E.g:
+         // type ModuleSetting =
+         //    {
+         //       pathType: "relative"
+         //       path: <string that must lead with './' or '../'>
+         //    } | {
+         //       pathType: "non-relative"
+         //       path: <string that must NOT lead with './' or '../'>
+         //    }
          // source and destination should be defined with paths relative to the project root, unless they are node_module imports
          {
             pathToOriginalModule: './src/db.ts',
