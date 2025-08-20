8e8fc564068d263e3d2c8317f236ca6cb24ca219
Adrian <adrianhyk@gmail.com>
2025-08-20 23:11:39 +0800

    remove deprecated function

--- src/lib/type-system/index.ts
+++ src/lib/type-system/index.ts
@@ -1,3 +1,2 @@
-export { getFunctionMeta as getFunctionMeta } from './getFunctionMetaPoc';
 export { generateSampleArgs } from './generateSampleArgsPoc';
 export { getFunctionJsDocs } from './getFunctionJsDocs';
