5704ad60006b96049d5ac45314cdebae165b325a
Adrian <adrianhyk@gmail.com>
2025-09-03 02:38:57 +0800

    migrate these tests from Jest to Vite

--- src/lib/sql/dialect-plugins/MssqlPlugin.test.ts
+++ src/lib/sql/dialect-plugins/MssqlPlugin.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import { MssqlPlugin } from './MssqlPlugin';
 
 describe('MssqlPlugin', () => {
--- src/lib/sql/dialect-plugins/PostgresPlugin.test.ts
+++ src/lib/sql/dialect-plugins/PostgresPlugin.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import { PostgresPlugin } from './PostgresPlugin';
 
 describe('PostgresPlugin', () => {
